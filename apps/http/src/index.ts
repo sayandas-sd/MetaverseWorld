b6eec05bbcd7e3f9d2b974e0526889639e2949f3
sayandas-sd <sayandasmac@gmail.com>
2025-01-08 15:41:21 +0530

    added nodenext module in ts-config

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import {router } from "./routes/v1"
-import client from "@repo/db"
+import client from "@repo/db/client"
+
 
 
 const app = express();
-
 
 app.use(express.json())
 
