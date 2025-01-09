71f625066f8ab91eced834079d05b891cb7a9d7b
sayandas-sd <sayandasmac@gmail.com>
2025-01-09 20:16:20 +0530

    added zod type

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import {router } from "./routes/v1"
-import client from "@repo/db/client"
-
 
 
 const app = express();
---
+++ apps/http/src/types/index.ts
@@ -0,0 +1,54 @@
+import z from "zod";
+
+export const SignupSchema = z.object({
+    username: z.string().email(),
+    password: z.string().min(5),
+    role: z.enum(["admin", "user"]),
+})
+
+export const SigninSchema = z.object({
+    username: z.string().email(),
+    password: z.string().min(5),
+})
+
+export const UpdateMetadata = z.object({
+    avatarId: z.string()
+})
+
+export const CreateSchema = z.object({
+    name: z.string(),
+    dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
+    mapId: z.string(),
+})
+
+export const AddElementSchema = z.object({
+    spaceId: z.string(),
+    elementId: z.string(),
+    x: z.number(),
+    y: z.number(),
+})
+
+export const CreateElementSchema = z.object({
+    imageUrl: z.string(),
+    height: z.number(),
+    static: z.boolean(),
+})
+
+export const UpdateElementSchema = z.object({
+    imageUrl: z.string(),
+})
+
+export const CreateAvatarSchema = z.object({
+    name: z.string(),
+    imageUrl: z.string()
+})
+
+export const CreateMapSchema = z.object({
+    thumbnail: z.string(),
+    dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
+    defaultElements: z.array(z.object({
+        elementId: z.string(),
+        x: z.number(),
+        y: z.number(),
+    }))
+})
