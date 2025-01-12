1c666436c1a46aac93f003cdde23c0b85f997a45
sayandas-sd <sayandasmac@gmail.com>
2025-01-12 20:15:45 +0530

    added auth route signup/signin

--- apps/http/src/routes/v1/auth.ts
+++ apps/http/src/routes/v1/auth.ts
@@ -1,11 +1,98 @@
 import { Router } from "express"
+import { SigninSchema, SignupSchema } from "../../types";
+import client from "@repo/db/client";
+import bcrypt from "bcrypt";
 
 export const authRouter = Router();
 
-authRouter.post("/signup", (req, res)=>{
+authRouter.post("/signup", async (req, res)=>{
 
+    const parseData = SignupSchema.safeParse(req.body)
+
+    if (!parseData.success) {
+        res.status(411).json({
+            message: "validation failed"
+        })
+        return;
+    }
+
+
+    try {
+
+        const userExits = await client.user.findUnique({
+            where: {
+                username: parseData.data.username
+            }
+        })
+
+        if(userExits) {
+            res.status(403).json({
+                message: "email is already taken"
+            })
+            return;
+        }
+
+        const salt = 10;
+        const hashPass = await bcrypt.hash(parseData.data.password, salt)
+
+        const newUser = await client.user.create({
+            data: {
+                username: parseData.data.username,
+                password: hashPass,
+                role: parseData.data.role === "admin" ?  "Admin" : "User",
+            }
+        })
+
+        res.status(200).json({
+            userId: newUser.id,
+        })
+        
+    }catch(e) {
+        res.status(500).json({
+            message: "server error"
+        })
+        return
+    }
 })
 
-authRouter.post("/signin", (req,res)=>{
-    
+authRouter.post("/signin", async (req,res)=>{
+
+    const parseData = SigninSchema.safeParse(req.body)
+
+    if(!parseData.success) {
+        res.status(411).json({
+            message: "validation failed"
+        })
+        return 
+    }
+
+    try {
+
+        const userExits = await client.user.findUnique({
+            where: {
+                username: parseData.data.username
+            }
+        })
+
+        if(!userExits) {
+            res.status(403).json({
+                message: "User not found"
+            })
+        }
+
+        const validPassword = bcrypt.compare(parseData.data.password, userExits.password)
+
+        if(!validPassword) {
+            res.status(403).json({
+                message: "Incorrect credential"
+            })
+        }
+
+    } catch(e) {
+        res.status(500).json({
+            message: "server error"
+        })
+        return
+    }
+
 })
